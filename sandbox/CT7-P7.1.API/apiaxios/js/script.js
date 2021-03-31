8424860a0ada41b8ba5ce0ddcfc311bd8980dec5
magomo <mgoncevatt.cep@gmail.com>
2021-03-31 19:03:48 +0200

    20210331.1903

---
+++ sandbox/CT7-P7.1.API/apiaxios/js/script.js
@@ -0,0 +1,66 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ONCLICK function =>
+
+function selectColor() {
+
+    const selectedColor = (document.getElementById("myColor").value).substring(1)
+
+    const baseurl = 'https://www.thecolorapi.com/id';
+
+    const params = '?format=json&hex=' + selectedColor;
+
+    console.log('api url: '+baseurl + params);
+
+    apiGetJsonClbk( baseurl + params, (dataapi) => {
+
+        if ( dataapi ) {
+
+            if ( !dataapi.code ) {
+
+                // OK show => El nom del color escollit en un <h1> i el contingut JSON que ha generat la consulta en un <p>
+                document.getElementById("elm_h1").innerHTML = "The name color is " + dataapi.name.value;
+                document.getElementById("elm_p").innerHTML = JSON.stringify(dataapi);
+
+            } else {
+
+                // KO show => Que hi ha hagut un error en un <h1>
+                document.getElementById("elm_h1").innerHTML = "API responds with error code " + dataapi.code;
+                document.getElementById("elm_p").innerHTML = dataapi.message;
+
+            }
+
+        } else {
+
+            document.getElementById("elm_h1").innerHTML = "An unexpected error occurred in the connection";
+            document.getElementById("elm_p").innerHTML = "";            
+
+        }
+
+    } );
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ONCLICK function //
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - AJAX function =>
+
+function apiGetJsonClbk( fullapilink, callbackfn ) {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+  
+    xhr.onreadystatechange = () => { if (xhr.readyState === XMLHttpRequest.DONE) { 
+
+        console.log( xhr.response );
+
+        callbackfn( xhr.response ); 
+    
+    } };
+
+    xhr.open( 'GET', fullapilink );
+
+    xhr.send();
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - AJAX function //
